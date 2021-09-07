849d5ee9707c797cf379cb3c10aa2f9f1235874a
Pthomas2 <pthomas1@lions.molloy.edu>
2021-09-07 16:11:37 -0400

    SPRING PROJECT

---
+++ jelp/src/main/java/com/cognixia/jump/springcloud/JelpApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JelpApplication.class, args);
+	}
+
+}
