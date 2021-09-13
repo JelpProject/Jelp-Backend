a70c8434a7676f5a1243b2e3272e52a42b99397e
nikkijwang <nikkijwang@gmail.com>
2021-09-12 18:34:03 -0700

    added some endpoints to test security

--- jelp/src/main/java/com/cognixia/jump/springcloud/controller/AuthController.java
+++ jelp/src/main/java/com/cognixia/jump/springcloud/controller/AuthController.java
@@ -6,12 +6,14 @@ import com.cognixia.jump.springcloud.service.MyUserDetailsService;
 import com.cognixia.jump.springcloud.util.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,11 @@ public class AuthController {
 
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
         
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
     }
 
 }
