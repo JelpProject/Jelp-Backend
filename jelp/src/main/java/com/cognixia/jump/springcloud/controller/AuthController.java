5241e6ada1960955679a3dae20f67febdb0da22f
Pthomas2 <pthomas1@lions.molloy.edu>
2021-09-14 11:13:00 -0400

    Update AuthController.java

--- jelp/src/main/java/com/cognixia/jump/springcloud/controller/AuthController.java
+++ jelp/src/main/java/com/cognixia/jump/springcloud/controller/AuthController.java
@@ -53,9 +53,5 @@ public class AuthController {
         
     }
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> sayHello() {
-        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
-    }
 
 }
