7648d99f22a4af929786e1121e46304d8d53beeb
nikkijwang <nikkijwang@gmail.com>
2021-09-14 02:01:42 -0700

    add controller for City model

---
+++ jelp/src/main/java/com/cognixia/jump/springcloud/controller/CityController.java
@@ -0,0 +1,88 @@
+package com.cognixia.jump.springcloud.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cognixia.jump.springcloud.model.City;
+import com.cognixia.jump.springcloud.repository.CityRepository;
+import com.cognixia.jump.springcloud.repository.StateRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+    
+    @Autowired
+    CityRepository cityRepo;
+
+    @Autowired
+    StateRepository stateRepo;
+
+    @GetMapping("/cities")
+    public Iterable<City> getAllCities() {
+        List<City> cities = cityRepo.findAll();
+
+        for (City city : cities) {
+            city.setCityState(stateRepo.getById(city.getStateId()));
+        }
+
+        return cities;
+        
+    }
+
+    @GetMapping("/city/{id}")
+    public City getCityById(@PathVariable Long id) {
+        Optional<City> found = cityRepo.findById(id);
+
+        if (found.isPresent()) {
+            City city = found.get();
+            city.setCityState(stateRepo.getById(city.getStateId()));
+            return city;
+        }
+
+        return null;
+    }
+
+    @PostMapping("/add/city")
+    public void addCity(@RequestBody City newCity) {
+        newCity.setCityId(-1L);
+        City added = cityRepo.save(newCity);
+
+        System.out.println("Added: " + added);
+    }
+
+    @PutMapping("/update/city")
+    public String updateCity(@RequestBody City updateCity) {
+        Optional<City> found = cityRepo.findById(updateCity.getCityId());
+
+        if (found.isPresent()) {
+            cityRepo.save(updateCity);
+            return "Saved: " + updateCity.toString();
+        }
+        else {
+            return "Could not update city, the id = " + updateCity.getCityId() + " doesn't exist";
+        }
+    }
+
+    @DeleteMapping("/delete/city/{id}")
+    public ResponseEntity<String> deleteState(@PathVariable Long id) {
+        Optional<City> found = cityRepo.findById(id);
+
+        if (found.isPresent()) {
+            cityRepo.deleteById(id);
+            return ResponseEntity.status(200).body("Deleted city with id = " + id);
+        }
+        else {
+            return ResponseEntity.status(400).body("City with id = " + id + " not found");
+        }
+    }
+
+}
--- jelp/src/main/java/com/cognixia/jump/springcloud/model/City.java
+++ jelp/src/main/java/com/cognixia/jump/springcloud/model/City.java
@@ -1,16 +1,13 @@
 package com.cognixia.jump.springcloud.model;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 
 @Entity
@@ -32,11 +29,6 @@ public class City implements Serializable {
 
     @Transient
     private State cityState;
-
-    @Transient
-    @OneToMany
-    @JoinColumn(name="Mbr_Id")
-    private List<Member> users;
 
     public City() {
         this(-1L, "N/A/", -1L, new State());
