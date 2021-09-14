a263254392f87bc1aef57a38fb1f30424368036f
nikkijwang <nikkijwang@gmail.com>
2021-09-14 01:13:12 -0700

    add anootations to the table columns to define relationship

--- jelp/src/main/java/com/cognixia/jump/springcloud/model/Review.java
+++ jelp/src/main/java/com/cognixia/jump/springcloud/model/Review.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Transient;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -36,9 +37,11 @@ public class Review implements Serializable {
 	@Column(name = "Rvw_Timestamp", columnDefinition = "timestamp default CURRENT_TIMESTAMP")
 	private LocalDateTime timeStamp;
 	
+	@ManyToMany
 	@Column(name = "Mbr_Id")
 	private Long mbrId;
 	
+	@ManyToMany
 	@Column(name = "Rest_Id")
 	private Long restaurantId;
 
