2c0466d60ab9c7e4013fef03cda6a3f96995365d
nikkijwang <nikkijwang@gmail.com>
2021-09-17 19:11:07 -0700

    remove method that's already defined by the super class

--- jelp/src/main/java/com/cognixia/jump/springcloud/repository/StateRepository.java
+++ jelp/src/main/java/com/cognixia/jump/springcloud/repository/StateRepository.java
@@ -1,7 +1,5 @@
 package com.cognixia.jump.springcloud.repository;
 
-import java.util.Optional;
-
 import com.cognixia.jump.springcloud.model.State;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StateRepository extends JpaRepository<State, Long>{
     State findByName(String name);
-    Optional<State> findById(Long id);
 }
