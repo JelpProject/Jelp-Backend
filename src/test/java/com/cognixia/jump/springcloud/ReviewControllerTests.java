5e0b2c7c867c1404f5ec16d7008d0b02b0bd3b9c
Pthomas2 <pthomas1@lions.molloy.edu>
2021-10-06 12:00:39 -0400

    removed unneeded import

--- src/test/java/com/cognixia/jump/springcloud/ReviewControllerTests.java
+++ src/test/java/com/cognixia/jump/springcloud/ReviewControllerTests.java
@@ -15,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
